f1458986b82dad2bcfbf193f7f123409050d498c
Anwender <Anwender@TSMIT-X240.fritz.box>
2019-04-16 15:09:09 +0200

    Neue Index-Datei

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <html>
     <head>
-        <meta charset="ISO-8859-1">
         <title></title>
     </head>
     <body>
         <?php
-        // put your code here
+        echo "Test Ausgabe";
         ?>
     </body>
 </html>
