3cf4045fc47e704187b13d21d8aa95f403b8ce45
Anwender <Anwender@TSMIT-X240.fritz.box>
2019-04-16 14:53:00 +0200

    No

---
+++ documentation/MatheZwei.php
@@ -0,0 +1,11 @@
+<?php
+
+
+/**
+ * Description of MatheZwei
+ *
+ * @author T.Smit
+ */
+class MatheZwei extends Calculator{
+    //put your code here
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
